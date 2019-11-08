1e15798f0ac5b30363c70d22183df198858f4958
emmajhau <ejhaughey@hotmail.com>
2019-11-09 02:03:53 +0900

    Changes to the code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -19,7 +19,11 @@ class Dec2Hex
                 System.exit(1);
             }
         }
-
+		else if (args = " ")
+		{
+			System.err.println("Must inclue input.");
+		}
+		
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
