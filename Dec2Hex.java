4afe534900460881f2e151595a128e16465f6156
emmajhau <ejhaughey@hotmail.com>
2019-11-09 02:10:53 +0900

    Further changes to the code, resolving errors

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -19,10 +19,10 @@ class Dec2Hex
                 System.exit(1);
             }
         }
-		else if (args = "0")
+		else if (args = " ")
 		{
 			System.err.println("Must include integer input.");
-		}gi
+		}
 		
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
